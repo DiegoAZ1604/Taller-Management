890632789e8faf33a50e0267e0ce81b39bfdf25a
DiegoAZ1604 <diegofuentes.a@hotmail.com>
2023-09-17 23:54:30 -0600

    Añadido de login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-const modoOscuroButton = document.getElementById('modo-oscuro');
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario de forma predeterminada
 
-modoOscuroButton.addEventListener('click', ()=>{
-    document.body.classList.toggle('modo-oscuro');
-})
+    // Obtiene los valores del usuario y la contraseña
+    var usuario = document.getElementById("usuario").value;
+    var contrasena = document.getElementById("contrasena").value;
+
+    // Verifica las credenciales (en este ejemplo, usuario=admin y contraseña=123)
+    if (usuario === "admin" && contrasena === "123") {
+        // Credenciales correctas, redirige a la página de reparaciones.html
+        window.location.href = "reparaciones.html";
+    } else {
+        // Credenciales incorrectas, muestra un mensaje de error o realiza otra acción
+        alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+    }
+});
